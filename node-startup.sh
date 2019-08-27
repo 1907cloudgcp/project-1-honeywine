4b3bff4f62cb6ae501ee5f785d0941559402f9b0
honeywine <joeplatina78@gmail.com>
2019-08-27 20:06:41 +0000

    08/27/2019

--- node-startup.sh
+++ node-startup.sh
@@ -1,9 +1,12 @@
+## change directory
+dir="`pwd`/`dirname "$0"`"
+cd ${dir}
+
 ## node server variables
-export SERVER_CERT="/home/joeplatina78/servercert.pem"
-export SERVER_KEY="/home/joeplatina78/serverkey.pem"
+export SERVER_CERT="${dir}/ssl_auth/servercert.pem"
+export SERVER_KEY="${dir}/ssl_auth/serverkey.pem"
 export SERVER_PASS="password"
 
 ## start server
-cd /home/joeplatina78/project-1-honeywine
 npm start
 
