bdc4a718564c50ba5bbb3c7e5e95f19006b19427
honeywine <joeplatina78@gmail.com>
2019-08-27 19:15:47 +0000

    08/27/2019

---
+++ node-startup.sh
@@ -0,0 +1,9 @@
+## node server variables
+export SERVER_CERT="/home/joeplatina78/servercert.pem"
+export SERVER_KEY="/home/joeplatina78/serverkey.pem"
+export SERVER_PASS="password"
+
+## start server
+cd /home/joeplatina78/project-1-honeywine
+npm start
+
